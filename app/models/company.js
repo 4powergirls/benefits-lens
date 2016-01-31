23c82ae14889c71611af8529cf6522a20f6caa0f
shri7287@gmail.com <vinay.rao@nuagenetworks.net>
2016-01-31 03:30:42 -0500

    Added basic server api to test mongo connection

--- app/models/company.js
+++ app/models/company.js
@@ -1,14 +1,9 @@
-// grab the mongoose module
-var mongoose = require('mongoose');
-var db = require('../../config/db');
+var mongoose = require("mongoose");
+var db = require("../../config/db");
 var dbmongo = mongoose.createConnection(db.url);
 
-var company = dbmongo.model('Company', {
-  id: {type: Number},
+var company = dbmongo.model("Company", {
   name: {type: String},
-  size: {min: Number, max: Number},
-  industry: {type: String, default: ""},
-  url: {type: String},
   maternityLeaveLength: {type: Number},
   paternityLeaveLength: {type: Number},
   paidSickLeave: {type: Number},
@@ -18,31 +13,12 @@ var company = dbmongo.model('Company', {
   paidMedicalInsurance: {type: Boolean},
   paidVisionInsurance: {type: Boolean},
   onsiteDayCare: {type: Boolean},
-  location: {type: String}
+  size: {
+    min: {type: Number},
+    max: {type: Number}
+  },
+  location: {type: String},
+  industry: {type: String}
 });
-
-
-company.create(
- {
-   id: 1,
-   name: 'Facebook',
-   size: {min: 10000, max: 20000},
-   industry: 'Internet',
-   url: 'http://facebook.com',
-   maternityLeaveLength: 16,
-   paternityLeaveLength: 16,
-   paidSickLeave: 16,
-   paidTimeOff: 16,
-   mothersRoom: true,
-   teleCommute: true,
-   paidMedicalInsurance: true,
-   paidVisionInsurance: true,
-   onsiteDayCare: true,
-   location: 94043
- },
-    function (err, small) {
-      if (err) return handleError(err);
-      // saved!
- });
 
 module.exports = company;
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,24 @@
 var Benefits = require('./models/benefits');
 var Company = require('./models/company');
 
-module.exports = function(app) {
+// get all benefits
+var getBenefits = function (res) {
+  Benefits.find(function(err, benefits) {
+    if (err) {
+      res.send(err);
+    }
+    res.json(benefits);  
+  });
+};
+
+module.exports = function (app) {
 
   // server routes ===========================================================
   // handle things like api calls
   // authentication routes
+  app.get("/api/benefits", function(req, res) {
+    getBenefits(res);
+  });
 
   // frontend routes =========================================================
   // route to handle all angular requests
